1bc042b27b3ff5bcc5ab1b304c7382c8e2f66ec7
unknown <you@example.com>
2019-04-07 11:51:35 +0530

    first commit

---
+++ src/test/java/mavenprojectpractice/mavenprojectclass.java
@@ -0,0 +1,11 @@
+package mavenprojectpractice;
+
+import org.testng.annotations.Test;
+
+public class mavenprojectclass {
+
+	@Test
+	public void printName(){
+		System.out.println("Rohan Raj Mittal");
+	}
+}
